d1d574504bcadda545f35fff04b5a071acf18b70
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 17:24:29 +0300

    Add Net.Chdk.Shared

--- src/Net.Chdk.Detectors.Software.Chdk/Properties/AssemblyInfo.cs
+++ src/Net.Chdk.Detectors.Software.Chdk/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,20 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Net.Chdk.Detectors.Software.Chdk")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dmitry Shechtman")]
-[assembly: AssemblyProduct("Net.Chdk")]
-[assembly: AssemblyCopyright("Copyright © 2017 Dmitry Shechtman")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0bbe7ef7-7715-4a9d-9f0c-2abe5f07ea33")]
 
 // Version information for an assembly consists of the following four values:
 //
